0980171e28566e902a06ae3cf332fe86f93a335d
udit-khanna <khannaudit@ymail.com>
2017-04-26 10:09:10 +0530

    Completed exercise-3

--- Panagram/src/keywords/PangramChecker.java
+++ Panagram/src/keywords/PangramChecker.java
@@ -1,25 +1,28 @@
 package keywords;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PangramChecker {
+	
+    private final Set missingLetters = new HashSet();  
 
 	public Boolean isPangram(String string) {
 		try {
-			String str = string.toLowerCase().replaceAll("[^a-z]", "");			
-				if(str.length() < 26){
-					return false;
-				}
-				else{
-					for(char ch = 'a'; ch <= 'z'; ch++){
-						if(!str.contains(String.valueOf(ch))){							
-							return false;
-						}
-					}
-				}
+			String str = string.toLowerCase();
+			for (char ch = 'a'; ch <= 'z'; ch++) {  
+	            missingLetters.add(ch);  
+	        }  
+	        for (int i = 0; i < str.length(); i++) {  
+	            missingLetters.remove(str.charAt(i));  
+	        } 
+	        return missingLetters.isEmpty();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		return false;
 	}
+  
 
 }
